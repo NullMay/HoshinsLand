efea8e636c87f5a4a5c2cca07a8cfef2c077591d
Nullton <74463225+Nullton@users.noreply.github.com>
2020-11-14 16:49:09 +0000

    Add files via upload

---
+++ HoshinsLand/src/main/java/com/Nullmay/HoshinsLand/HoshinsLandApplication.java
@@ -0,0 +1,13 @@
+package com.Nullmay.HoshinsLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoshinsLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoshinsLandApplication.class, args);
+	}
+
+}
---
+++ HoshinsLand/src/test/java/com/Nullmay/HoshinsLand/HoshinsLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nullmay.HoshinsLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoshinsLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
