e2fcd7a8af02ddf732d38c8bb9395dd3241d9a33
frimanim <frimanik@gmail.com>
2021-03-11 16:09:31 +0500

    Added few entities, didn't completely described them yet.

--- src/main/java/com/Nullmay/HoshinsLand/ServletInitializer.java
+++ src/main/java/com/Nullmay/HoshinsLand/ServletInitializer.java
@@ -3,9 +3,9 @@ package com.Nullmay.HoshinsLand;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class
+ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(HoshinsLandApplication.class);
 	}
