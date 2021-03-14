734903f778c6f3614231ab9614ceebdd8341f5f9
frimanim <frimanik@gmail.com>
2021-03-14 22:56:04 +0500

    Fixed login.

--- src/main/java/com/Nullmay/HoshinsLand/Config/EncoderConfig.java
+++ src/main/java/com/Nullmay/HoshinsLand/Config/EncoderConfig.java
@@ -3,8 +3,9 @@ package com.Nullmay.HoshinsLand.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class EncoderConfig {
 
     @Bean
